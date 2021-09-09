80dbb2eb9df15085176ef81754dd20e3451b87b2
Martin Claesson <martin@claesson.co>
2021-09-10 00:59:20 +0200

    #37 #1 #2 Added help and play with the base command layout

---
+++ src/commands/CommandTemplate.ts
@@ -0,0 +1,11 @@
+import {Command} from "../types/Command";
+import { Message} from "discord.js";
+import {Client} from "../types/Client";
+
+export default new Command("Command", "Commanding Command", exec)
+
+function exec(client: Client, message: Message, args: string):Promise<string> {
+    return new Promise((resolve,reject) => {
+        resolve("")
+    })
+}
---
+++ src/commands/core/Help.ts
@@ -0,0 +1,11 @@
+import {Command} from "../../types/Command";
+import {Message} from "discord.js";
+import {Client} from "../../types/Client";
+
+export default new Command("Help", "Shows this message", exec)
+
+function exec(client: Client, message: Message, args: string):Promise<string> {
+    return new Promise((resolve,reject) => {
+        resolve("")
+    })
+}
